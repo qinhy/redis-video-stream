bf2695fefa8b26ca2948a6df75f44c390f288130
HUANGYI QINH <545705507@qq.com>
2024-05-29 17:56:51 +0900

    Update celery_task.py

--- celery_task.py
+++ celery_task.py
@@ -40,6 +40,48 @@ def WrappTask(task:Task):
     task.progress = update_progress_state
     task.error = update_error_state
     return task 
+# Import the Celery application
+from celery_app import app
+import cv2
+import redis
+import time
+from urllib.parse import urlparse
+
+class SimpleMovingAverage(object):
+    def __init__(self, value=0.0, count=7):
+        self.count = int(count)
+        self.current = float(value)
+        self.samples = [self.current] * self.count
+
+    def add(self, value):
+        v = float(value)
+        self.samples.insert(0, v)
+        o = self.samples.pop()
+        self.current = self.current + (v-o)/self.count
+
+class Video:
+    def __init__(self, infile=0, fps=30.0):
+        self.isFile = not str(infile).isdecimal()
+        self.ts = time.time()
+        self.infile = infile
+        self.cam = cv2.VideoCapture(self.infile)
+        if not self.isFile:
+            self.cam.set(cv2.CAP_PROP_FPS, fps)
+            self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
+            self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
+        else:
+            self.fps = self.cam.get(cv2.CAP_PROP_FPS)
+            self.sma = SimpleMovingAverage(value=0.1, count=19)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        ret_val, img0 = self.cam.read()
+        if not ret_val:
+            raise StopIteration()
+        img = cv2.flip(img0, 1) if not self.isFile else img0
+        return img
 
 class CeleryTaskManager:    
     @staticmethod
@@ -49,4 +91,19 @@ class CeleryTaskManager:
         return celery_app.control.revoke(task_id, terminate=True)
     
     @staticmethod
-    @celery_app.task(bind=True)
+    @celery_app.task(bind=True)    
+    def capture_video(t: Task, infile, fps, output, url, fmt='.jpg', maxlen=1000, count=None):
+        url = urlparse(url)
+        conn = redis.Redis(host=url.hostname, port=url.port)
+        loader = Video(infile=infile, fps=fps)
+        for i, img in enumerate(loader):
+            if count is not None and i >= count:
+                break
+            _, data = cv2.imencode(fmt, img)
+            msg = {
+                'count': i,
+                'image': data.tobytes()
+            }
+            _id = conn.xadd(output, msg, maxlen=maxlen)
+            print('frame:', i, 'id:', _id)
+
